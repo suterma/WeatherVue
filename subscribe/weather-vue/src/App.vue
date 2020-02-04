dc0d964d8d8feb85db672d89b692b96441157c57
Marcel <gitkraken@marcelsuter.ch>
2020-02-05 00:21:51 +0100

    Added MQTT test client

--- subscribe/weather-vue/src/App.vue
+++ subscribe/weather-vue/src/App.vue
@@ -2,16 +2,22 @@
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <sub1></sub1>
   </div>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
+import Sub1 from './components/Sub1'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    HelloWorld,
+    Sub1
+  },
+  mounted () {
+    this.$mqtt.subscribe('testtopic/1')
   }
 }
 </script>
@@ -24,5 +30,15 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+.sub {
+  width: 31%;
+  float: left;
+  border: 1px solid #ccc;
+  margin: 20px 1%;
+  padding: 20px 0;
+}
+button {
+  padding: 10px 20px;
 }
 </style>
--- subscribe/weather-vue/src/main.js
+++ subscribe/weather-vue/src/main.js
@@ -1,5 +1,9 @@
 import Vue from 'vue'
 import App from './App.vue'
+import VueMqtt from 'vue-mqtt'
+
+//TODO Firefox can’t establish a connection to the server at ws://test.mosquitto.org/.
+Vue.use(VueMqtt, 'ws://test.mosquitto.org:80', {clientId: 'WebClient-' + parseInt(Math.random() * 100000)})
 
 Vue.config.productionTip = false
 
